d15a3a229539b426f6c3350b3f4bd99e6c2b75a5
Mateusz Urban <mateuszurban2@gmail.com>
2024-06-10 16:38:23 +0200

    wip

--- cps.hs
+++ cps.hs
@@ -6,7 +6,7 @@ sum' (x:xs) = x + sum xs
 
 sumCps :: [Int] -> (Int -> Int) -> Int
 sumCps [] k = k 0
-sumCps (x:xs) k = sumCps xs (\y -> x + k y)
+sumCps (x:xs) k = sumCps xs (\y -> k $ x + y)
  
 -- sumCps [1,2,3] (\x -> x) -> sumCps [2,3] (\y -> 1 + (\y -> y)) -> sumCps [3] (\y -> 2 + (1 ))
 --
@@ -20,4 +20,20 @@ fact n = n * fact (n-1)
 factCps :: Int -> (Int -> Int) -> Int
 factCps 0 k = k 1
 factCps n k = factCps (n - 1) (\y -> n * k y)
+
+fib :: Int -> Int
+fib n 
+  | n == 0 = 0
+  | n == 1 = 1
+  | otherwise = fib (n-2) + fib (n - 1)
+
+fibCps :: Int -> (Int -> Int) -> Int
+fibCps n k 
+  | n <= 0    = k 0
+  | n == 1    = k 1
+  | otherwise = fibCps (n - 2) (\x -> fibCps (n - 1) (\y -> k (x + y)))
+
+cpsLen :: [a] -> (Int -> Int) -> Int
+cpsLen [] k = k 0
+cpsLen (x:xs) k = cpsLen xs (\y -> k (1 + y))
 
